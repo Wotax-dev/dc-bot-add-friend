bfadb7b4b3a264dc53d37ade9b286aa314dd39ab
Wotax? <santoshdonna2@gmail.com>
2025-11-23 19:15:42 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 # main.py
-from bot import run
+from threading import Thread
+from bot import run as run_bot
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Discord bot is running!", 200
+
+def run_flask():
+    app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
-    run()
+    Thread(target=run_flask).start()
+    run_bot()
